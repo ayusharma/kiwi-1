2e61cca03bb3af2e1cff2780c251bc14907875a8
Ayush Sharma <ayush.sharma@innovaccer.com>
2015-06-01 20:45:30 +0530

    JSTL Added

--- src/org/oncoblocks/web/controllers/PrimaryController.java
+++ src/org/oncoblocks/web/controllers/PrimaryController.java
@@ -1,13 +1,16 @@
 package org.oncoblocks.web.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PrimaryController {
 	
 	@RequestMapping("/")
-	public String showHome(){
+	public String showHome(Model model){
+		
+		model.addAttribute("name","Ayush");
 		 return "home";
 	}
 }
